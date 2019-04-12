0aa8c94da8c0404874242e6646ab48e20da51c03
Deary <chickenflakes9@gmail.com>
2019-04-12 21:31:55 +0700

    Connect to database

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ const exphbs = require ('express-handlebars');
 const bodyParser = require ('body-parser');
 const path = require ('path');
 
+// Database
+const db = require('./config/database');
+
+// Test db
+db.authenticate()
+  .then(() => console.log('Database Connected..'))
+  .catch(err => console.log('Error: ' +err))
+
 const app = express();
 
 app.get('/', (req, res) => res.send('Index'));
+
+// Gig  Routes
+app.use('/gigs', require ('./routes/gigs'));
 
 const PORT = process.env.PORT || 5000;
 
