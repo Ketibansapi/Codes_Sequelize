682377abffc9a58520948a818ea72ceecbaba7f7
Deary <chickenflakes9@gmail.com>
2019-04-14 03:48:32 +0700

    add and search

--- routes/gigs.js
+++ routes/gigs.js
@@ -2,15 +2,15 @@ const express = require ('express');
 const router = express.Router();
 const db = require('../config/database');
 const Gig = require('../models/Gig');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 //Get gig list
 router.get('/', (req, res) =>
   Gig.findAll()
-  .then(gigs => {
-    res.render('gigs', {
+  .then(gigs => res.render('gigs', {
       gigs
-    });
-  })
+    }))
   .catch(err => console.log(err)));
 
 // Display add gig form
@@ -18,26 +18,65 @@ router.get('/add', (req, res) => res.render('add'));
 
 //Add a gigs
 router.post('/add', (req, res) => {
-  const data = {
-    title: 'Netlify Expert',
-    technologies: 'javascript',
-    budget: '$3000',
-    description: ' mantappppppppppppp hehehehe',
-    contact_email: 'user@gmail.com'
+  let {title, technologies, budget, description, contact_email} = req.body;
+  let errors = [];
+
+  // Validate Fields
+  if(!title) {
+    errors.push({ text: 'Please add a title' });
+  }
+  if(!technologies) {
+    errors.push({ text: 'Please add technologies' });
+  }
+  if(!description) {
+    errors.push({ text: 'Please add description' });
+  }
+  if(!contact_email) {
+    errors.push({ text: 'Please add an email' });
   }
 
-  let {title, technologies, budget, description, contact_email} = data;
+  // Check for errors
+  if(errors.length > 0) {
+    res.render('add', {
+      errors,
+      title,
+      technologies,
+      budget,
+      description,
+      contact_email
+    });
+  } else {
+    if(!budget) {
+      budget = 'Unknown';
+    } else {
+        budget = `$${budget}`;
+    }
 
-  // Insert to table
-  Gig.create ({
-    title,
-    technologies,
-    description,
-    budget,
-    contact_email
-  })
-    .then(gig => res.redirect('/gigs'))
-    .catch(err => console.log(err));
+    // lowercase and Space after comma
+    technologies = technologies.toLowerCase().replace(/, /g, ',');
+
+    // Insert to table
+      Gig.create ({
+        title,
+        technologies,
+        description,
+        budget,
+        contact_email
+      })
+      .then(gig => res.redirect('/gigs'))
+      .catch(err => console.log(err));
+  }
+});
+
+// search
+router.get('/search', (req, res) => {
+  let { term } = req.query;
+
+  term = term.toLowerCase();
+
+  Gig.findAll({ where: { technologies: { [Op.like]: '%' + term + '%' }}})
+  .then(gigs => res.render ('gigs', { gigs }))
+  .catch(err => console.log(err));
 });
 
 module.exports = router;
